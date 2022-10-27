f8ecb00d2d63c386a2f9fe781eda95fd28f19f52
carmauc <carmauc339@hotmail.com>
2022-10-26 19:32:40 -0500

    poo2

--- Proyecto1-node/Ejercicios/poo.js
+++ Proyecto1-node/Ejercicios/poo.js
@@ -1,20 +1,19 @@
 // Crea un nuevo fichero JS que contenga las siguientes líneas
 
 // - Una clase llamada "Estudiante" que tenga:
-
-
-class Estudiante {
-    constructor(nombre, asignaturas) {
-        this.nombre = nombre
-        this.asignaturas = ["javascript", "HTML", "CSS"]
-    }
-obtendatos(){ this.nombre, this.asignaturas}
 // - Una variable llamada nombre
-
 // - Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
-
 // - Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+class Estudiante {
+    nombre = "Mauricio"
+    asignaturas = ["javascript", "HTML", "CSS"]
+    obtendatos(){ 
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas
+        }}}
 
 // - Crea una nueva instancia de "Estudiante"
-
-// - Haz la llamada al método obtenDatos
+const Estudiante = new estudiante ()
+// - Haz la llamada al método obtenDato
+console.log(estudiante.obtendatos());
