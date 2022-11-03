22d939d2a09bf4144608c6c1b42c24ed4a7f7bf2
carmauc <carmauc339@hotmail.com>
2022-11-03 12:13:11 -0500

    maps

--- Proyecto1-node/Ejercicios/funciones_ejercicios.js
+++ Proyecto1-node/Ejercicios/funciones_ejercicios.js
@@ -2,18 +2,18 @@
 
 // - Una función sin parámetros que devuelva siempre "true"
 function devuelveTrue() {
-    return true
+  return true;
 }
 // - Una función asíncrona qe utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
 
 async function damePromesa() {
-    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+  return setTimeout(() => console.log("Hola soy una promesa"), 5000);
 }
 // - Una función generadora de índices pares automáticos
 
 function* idsPares() {
-    let id = 0
-    while(true) {
-        yield id += 2  
-    }
+  let id = 0;
+  while (true) {
+    yield (id += 2);
+  }
 }
